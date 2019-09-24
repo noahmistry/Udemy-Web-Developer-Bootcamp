6f5ffc451b28c6a2e086be5359b5017b8ac5d642
noahmistry <noahmistry@penguin>
2019-09-24 12:48:34 -0700

    - array problem set solution complete -

--- JS/Arrays/Array Problem Set/script.js
+++ JS/Arrays/Array Problem Set/script.js
@@ -1,12 +1,12 @@
-// var numbers = [1,2,3,4,5,6,7,8,9,10];
-
+// *** Start of printReverse function ***//
 //create printReverse function
-// function printReverse(arr) {
-//     for (var i = arr.length - 1; i >= 0; i--) {
-//         console.log(arr[i]); 
-//     }
-// }
-// console.log(printReverse([2,3,4,5,6]));
+function printReverse(arr) {
+    for (var i = arr.length - 1; i >= 0; i--) {
+        console.log(arr[i]); 
+    }
+}
+console.log(printReverse([2,3,4,5,6]));
+
 
 // *** Start of isUniform function *** //
 
@@ -22,17 +22,38 @@ function isUniform(arr) {
 }
 console.log(isUniform([1,1,1,1]));
 
-// Alternative Solution with forEach loop //
-function isUniform(arr) {
-    var first = arr[0];
-    arr.forEach(function(element) {
-        if (element !== first) {
-            return false;
-        }
-    });
-    return true;    
-}
+// // Alternative Solution with forEach loop //
+// function isUniform(arr) {
+//     var first = arr[0];
+//     arr.forEach(function(element) {
+//         if (element !== first) {
+//             return false;
+//         }
+//     });
+//     return true;    
+// }
 // This solution will not work because after the 'return false' is executed the forEach loop is ended not the whole function isUniform. So we will always see a return of true //
 
 
 // *** Start is sumArray function ***//
+
+function sumArray(numbersArray) {
+      var total = 0;
+      numbersArray.forEach(function(arr) {
+          total += arr;
+      });
+      return total;
+}
+    console.log(sumArray([20,10,10]));
+
+// *** Start max() array function ***//
+function max(arr) {
+    var maxNumber = arr[0]; 
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > maxNumber) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(max([3,4,5]));
