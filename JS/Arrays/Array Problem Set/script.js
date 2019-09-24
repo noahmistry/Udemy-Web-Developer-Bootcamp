4e7eae3b9f913866c78fe5d10c635dec62ccfed1
noahmistry <noahmistry@penguin>
2019-09-24 10:32:31 -0700

    - Array problem set solution -

---
+++ JS/Arrays/Array Problem Set/script.js
@@ -0,0 +1,38 @@
+// var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+//create printReverse function
+// function printReverse(arr) {
+//     for (var i = arr.length - 1; i >= 0; i--) {
+//         console.log(arr[i]); 
+//     }
+// }
+// console.log(printReverse([2,3,4,5,6]));
+
+// *** Start of isUniform function *** //
+
+function isUniform(arr) {
+    //create a var to compare with other items in the array
+    var first = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] !== first) {
+            return false;
+        }        
+    }
+    return true;
+}
+console.log(isUniform([1,1,1,1]));
+
+// Alternative Solution with forEach loop //
+function isUniform(arr) {
+    var first = arr[0];
+    arr.forEach(function(element) {
+        if (element !== first) {
+            return false;
+        }
+    });
+    return true;    
+}
+// This solution will not work because after the 'return false' is executed the forEach loop is ended not the whole function isUniform. So we will always see a return of true //
+
+
+// *** Start is sumArray function ***//
