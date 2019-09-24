bfa327bcc0ec071871082aa215abf06bf80e2f78
noahmistry <noahmistry@penguin>
2019-09-23 22:36:54 -0700

    Revert "added two functions delete& show index of item. Refactored code to three functions"
    This reverts commit 9eb9dbf834627110dff6dc53de32db7fc9d612fe.

--- JS/Arrays/Todo List/script.js
+++ JS/Arrays/Todo List/script.js
@@ -5,37 +5,13 @@ var list = [];
 var input = prompt("What would you like to add to the list?");
 while (input != "quit") {
     if (input === "list") {
-        listTodos();
         console.log(list);
     } else if (input === "new") {
-        addTodo();
-    } else if (input === "delete") {
-       deleteTodo();
-    }
-    //ask again for new input
-    var input = prompt("What would you like to add to the list?");
-    }
-    console.log("You have quit the app, Goodbye!");
-
-    function listTodos() {
-        list.forEach(function(todoItem, i) {
-            console.log("----------------");
-            console.log(i + ": " + todoItem);
-        })
-        console.log("----------------");
-    }
-
-    function addTodo() {
         var newTodo = prompt("Enter a new list item");
         list.push(newTodo);
-        console.log("Added todo!")
     }
+    var input = prompt("What would you like to add to the list?");
+}
+    console.log("You have quit the app, Goodbye!");
 
-    function deleteTodo() {
-         // ask for index of the todo to be deleted
-         var indexDelete = prompt("Which index would you like to be deleted?");
-         // delete the indexed todo
-         // splice
-         list.splice(indexDelete,1);
-         console.log("Todo successfully deleted!");
-    }
+    
