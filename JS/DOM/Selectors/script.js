497fefe346edddc7f9fe00090120834753e4a91c
noahmistry <noahmistry@penguin>
2019-09-27 17:46:39 -0700

    JS-DOM Selectors -

---
+++ JS/DOM/Selectors/script.js
@@ -0,0 +1,12 @@
+// get Element by ID //
+document.getElementById("first");
+// get by Class name //
+document.getElementsByClassName("special")[0];
+// get Element ID by Query Selector //
+document.querySelector("#first");
+// Query Selector First Element //
+document.querySelector("p");
+// Query Selector All //
+document.querySelectorAll(".special")[0]
+//  Tag name //
+document.getElementsByTagName("p")[0];
