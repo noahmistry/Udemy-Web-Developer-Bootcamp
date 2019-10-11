1e6fb4b27ca424b568f7bed2b0bc74f86d0937ce
noahmistry <noahmistry@penguin>
2019-10-11 10:53:31 -0700

    -jQuery - Selector Exercise Solution-

---
+++ JS/jQuery/Selector Exercise/app.js
@@ -0,0 +1,5 @@
+$("div").css("background-color","purple");
+$(".highlight").css("width","200px");
+$("#third").css("border","4px solid orange");
+// select only the first div and change font color to orange //
+$("div:first-of-type").css("color","orange");
